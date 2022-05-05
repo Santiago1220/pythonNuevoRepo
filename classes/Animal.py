4513eaf1a0413eb23787c6f8f850a31f7b169962
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-05-05 07:48:28 -0500

    Se crea segundo commit

--- classes/Animal.py
+++ classes/Animal.py
@@ -4,3 +4,6 @@ class Animal:
         self.peso = None
         self.alimento = None
         self.tipo = None
+    
+    def saludar(self):
+        print("Hola, mi nombre es frailejon Hernesto Perez")
